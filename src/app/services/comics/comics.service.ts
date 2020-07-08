9929406987240b9a042ef347dab2593492cd42d9
Raul <raulcc50@gmail.com>
2020-07-07 23:28:41 -0300

    refact: adicionando endpoint

--- src/app/services/comics/comics.service.ts
+++ src/app/services/comics/comics.service.ts
@@ -4,11 +4,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import Comic from 'src/app/models/comic.model';
+import Creator from 'src/app/models/creator.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComicsService {
+
+  id: number;
 
   private readonly API = `${environment.marvelAPI.baseAPI}/comics${environment.marvelAPI.apiKey}`;
 
@@ -19,5 +22,10 @@ export class ComicsService {
   getAllComics(): Observable<Comic[]> {
     return this.http.get<Comic[]>(this.API).pipe(map((res: any) => res.data.results));
   }
+
+  getCreators(id: number): Observable<Creator[]> {
+    return this.http.get<Creator[]>(`${environment.marvelAPI.baseAPI}/comics/${id}/${environment.marvelAPI.creators}`).pipe(map((res: any) => res.data.results));
+  }
+
 
 }
